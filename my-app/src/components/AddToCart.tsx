2e8931f6bb24fc64ce2c9e3db43ca322916f7776
Ripudaman Bansal <ripudamanbansal20@gmail.com>
2025-04-08 21:23:42 +0530

    learn data fetching

---
+++ my-app/src/components/AddToCart.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <>
+      <button
+        onClick={() => {
+          console.log("clicked");
+        }}
+      >press</button>
+    </>
+  );
+};
+
+export default ProductCard;
